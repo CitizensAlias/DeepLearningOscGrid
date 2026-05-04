ff406c13d90e0bb27d8fc56b1834fe669a2b897a
javaGoException <>
2026-05-04 18:37:27 +0200

    - 3 sets WiP

--- main.py
+++ main.py
@@ -6,16 +6,23 @@ from catboost import CatBoostClassifier
 print("loading dataset...")
 
 # run divider.py first to get the simplified labels
-labeled_dataset = pd.read_csv('data/labeled_processed.csv')
+labeled_dataset = pd.read_csv('data/labeled_processed_simplified.csv')
 
 print("dataset loaded!")
 
-train, test = train_test_split(
+train, temp = train_test_split(
     labeled_dataset,
-    test_size=0.2,
+    test_size=0.4,
     random_state=1,
     stratify=labeled_dataset['event_type']
 )
+val, test = train_test_split(
+    temp,
+    test_size=0.5,
+    random_state=1,
+    stratify=temp['event_type']
+)
+
 
 fields_to_drop = [
     'sample', 'file_name', 'ML_1', 'ML_1_1', 'ML_1_1_1', 'ML_1_2','ML_2','ML_2_1_1','ML_2_1_2','ML_2_1_3','ML_2_2','ML_2_3',
@@ -24,6 +31,9 @@ fields_to_drop = [
 
 train_features = train.drop(fields_to_drop, axis=1)
 train_labels = train['event_type']
+
+val_features = val.drop(fields_to_drop, axis=1)
+val_labels = val['event_type']
 
 test_features = test.drop(fields_to_drop, axis=1)
 test_labels = test['event_type']
@@ -39,7 +49,10 @@ print("training...")
 
 classifier.fit(
     train_features, train_labels,
-    eval_set=(test_features, test_labels),
+    eval_set=(val_features, val_labels),
     verbose=1,
     #plot=True
 )
+
+test_score = classifier.score(test_features, test_labels)
+print(f"Test accuracy: {test_score}")
